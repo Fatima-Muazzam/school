95d1ee7d46a3a37665cc42606aeb427c7a61bdc7
Your Name <you@example.com>
2023-01-05 02:37:52 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+document.getElementById("submit").onclick = function() {
+  alert('Thank you for your responsive. We will contact you soon');
+  window.location.href = "index.html";
+};
